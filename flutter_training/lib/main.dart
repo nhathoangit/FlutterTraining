e21c9919e95186dcbc810b2783c73ee411d93962
Anh Tien <tien.nguyen@apps-cyclone.com>
2019-04-17 09:46:15 +0700

    Demo Text and AppBar

--- flutter_training/lib/main.dart
+++ flutter_training/lib/main.dart
@@ -9,103 +9,164 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TextPage(),
     );
   }
+
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// Demo AppBar
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+// TODO Simple AppBar with Center Title
+/*
+ AppBar (
+    backgroundColor: Colors.blue,
+    title: new Text("Title"),
+    elevation: 4.0,
+    centerTitle: true,
+),*/
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+// TODO Simple ApBar with IconTheme and TextTheme
+/*
+    AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: new Text("Title"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.search),
+              onPressed: (){},
+              ),
+        ],
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        textTheme: TextTheme(
+          title: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          )
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+*/
+
+// TODO AppBar with List Of Action Button
+/*     AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: new Text("Title"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.search),
+              onPressed: (){},
+          ),
+          new IconButton(icon: new Icon(Icons.menu),
+            onPressed: (){},
+          ),
+        ],
+      ),
+*/
+
+
+// TODO AppBar with Title and SubTitle at Center
+
+/*    AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.blueAccent,
+        title: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                "Title",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              new Text(
+                "SubTile",
+                style: TextStyle(fontSize: 14.0),
+              )
+            ],
+          ),
+        ),
+      ),*/
+
+
+
+// Demo Text
+class TextPage extends StatefulWidget {
+  @override
+  _TextState createState() => _TextState();
+}
+
+class _TextState extends State<TextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: new Text("Title"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.search),
+            onPressed: (){},
+          ),
+        ],
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        textTheme: TextTheme(
+            title: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            )
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Text(
+          "TextStyle with easy parameters",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 22.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w500, // Medium
+            background: Paint()
+              ..color = Color(0xffc7e5b4)
+              ..style = PaintingStyle.fill,
+            letterSpacing: 4,
+            wordSpacing: 20, //
+          ),
+        )
+      ),
     );
   }
+}
+
+// Demo Row
+
+class RowPage extends StatefulWidget {
+  @override
+  _RowState createState() => _RowState();
+}
+
+class _RowState extends State<RowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 7,
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Container(
+            color: Colors.yellow,
+          ),
+        ),
+      ],
+    );
+  }
+
 }
