8d2794b8be58d2d8b643f341b43180d6113c56d3
Anh Tien <tien.nguyen@apps-cyclone.com>
2019-04-22 11:00:36 +0700

    State Life cycle Demo

--- flutter_training/lib/main.dart
+++ flutter_training/lib/main.dart
@@ -21,6 +21,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+
+  _MyHomePageState() {
+    print('Demo State');
+  }
+  
   @override
   void initState() {
     WidgetsBinding.instance.addObserver(this);
@@ -30,7 +35,14 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
+    print("From state dispose");
     super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(MyHomePage oldWidget) {
+    print("From state didUpdateWidget");
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
@@ -39,7 +51,14 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
       appBar: AppBar(
         title: Text('Demo Lifecycle'),
       ),
-      body: Center(),
+      body: RaisedButton(
+          child: Text("Go to next"),
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) {
+              return LifeCycleDemo();
+            }));
+          }),
     );
   }
 
@@ -48,4 +67,68 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     print('State = $state');
   }
 
+}
+
+class LifeCycleDemo extends StatefulWidget {
+  LifeCycleDemo() {
+    print("Widget constructor");
+  }
+
+  @override
+  _LifeCycleDemoState createState() => _LifeCycleDemoState();
+}
+
+class _LifeCycleDemoState extends State<LifeCycleDemo> {
+  int counter = 0;
+
+  _LifeCycleDemoState() {
+    print("Widget state constructor");
+  }
+
+  @override
+  void initState() {
+    print("From initState");
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(LifeCycleDemo oldWidget) {
+    print("From state didUpdateWidget");
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("From state build");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful Widget Lifecycle Demo"),
+      ),
+      body: Container(
+        child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Counter: $counter",
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter++;
+                    });
+                  },
+                  child: Text("Click Me"),
+                )
+              ],
+            )),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    print("From state dispose");
+    super.dispose();
+  }
 }
